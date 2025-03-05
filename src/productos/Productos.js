aea82f51aa75bf85ce5924a40b3ca4cb2826fa03
carri8 <mcarri17@ucm.es>
2025-03-05 17:54:45 +0100

    Cambios para poder registrar un producto

--- src/productos/Productos.js
+++ src/productos/Productos.js
@@ -4,35 +4,36 @@ export class Producto {
     static #insertStmt = null;
     static #updateStmt = null;
 
-    #nombre;
-    #descripcion;
-    #precio;
-    #imagen;
+    nombre;
+    descripcion;
+    precio;
+    imagen;
+    #id
+    #user_id
 
-    constructor(nombre, descripcion, precio,imagen) {
-        this.#nombre = nombre;
-        this.#descripcion = descripcion;
-        this.#precio = precio;
-        this.#imagen = imagen;
-
+    constructor(nombre, descripcion, precio, imagen, id = null) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.#id = id
     }
 
     static initStatements(db) {
         if (this.#getByUserIdStmt !== null) return;
-        // TODO
-        this.#getByUserIdStmt = db.prepare('');
-        this.#insertStmt = db.prepare('');
-        this.#updateStmt = db.prepare('');
+        this.#getByUserIdStmt = db.prepare('SELECT id, nombre, precio, imagen FROM productos WHERE user_id = @user_id');
+        this.#insertStmt = db.prepare('INSERT INTO productos(nombre, descripcion, precio, imagen) VALUES (@nombre, @descripcion, @precio, @imagen)');
+        this.#updateStmt = db.prepare('UPDATE productos SET nombre = @nombre, descripcion = @descripcion, precio = @precio, imagen = @imagen');
         this.#getAllStmt = db.prepare('SELECT nombre, precio FROM productos');
     }
 
     static getProductByUserId(user_id) {
             const producto = this.#getByUserIdStmt.get({ user_id });
-            if (producto === undefined) throw new UsuarioNoEncontrado(user_id);
+            if (producto === undefined) throw new ProductoNoEncontrado(nombre);
     
-            const { id, password, nombre,rol} = usuario;
-            console.log('Usuario recibido de la BD:', usuario);
-            return new Usuario(username, password, nombre, rol, id);
+            const { id, password, nombre,rol} = producto;
+            console.log('Producto recibido de la BD:', producto);
+            return new Producto(nombre, descripcion, precio, imagen);
         }
     static getProducts() {
         const productos = this.#getAllStmt.all();
@@ -45,8 +46,20 @@ export class Producto {
         console.log('Creando producto:', nombre);
         const Tproducto = new Producto(nombre, descripcion, precio,imagen);
         Tproducto.nombre = nombre;
-        console.log('Producto creado:', Tproducto.#nombre);
+        console.log('Producto creado:', Tproducto.nombre);
         return Tproducto;
     }   
 
+}
+
+export class ProductoNoEncontrado extends Error {
+    /**
+     * 
+     * @param {string} nombre 
+     * @param {ErrorOptions} [options]
+     */
+    constructor(nombre, options) {
+        super(`Producto no encontrado: ${nombre}`, options);
+        this.name = 'ProductoNoEncontrado';
+    }
 }
