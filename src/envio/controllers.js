810e9c8a77d5d06d649ddcf23f9c6db4c6abc7a0
Agaharo1 <115159667+Agaharo1@users.noreply.github.com>
2025-04-01 22:23:24 +0200

    Punto de recogida funcional
    Me ha costado mazo

--- src/envio/controllers.js
+++ src/envio/controllers.js
@@ -7,16 +7,32 @@ import { Imagen } from "../imagenes/Imagen.js";
 
 export function formularioPuntoRecogida(req, res) {
     const { id } = req.params;
+    console.log("ID del producto recibido:", id);
+    const producto = Producto.getProductById(id);
+    console.log(producto);
     const puntoRecogida = PuntoRecogida.getPuntoRecogidaById(id);
     const params = {
       contenido: "paginas/envios/formPuntoRecogida",
       session: req.session,
+      producto,
       puntoRecogida
       
     };
     res.render("pagina", params);
   
   }
+
+  export function mostrarPuntoRecogida(req, res) {
+    const { puntoId,productoId } = req.body;
+    console.log("Producto ID recibido:", productoId);
+    try {
+        const puntoRecogida = PuntoRecogida.getPuntoRecogidaByOneId(puntoId);
+        req.session.puntoRecogidaSeleccionado = puntoRecogida;
+        res.redirect(`/envios/resumenProducto/${productoId}`);
+    } catch (error) {
+        res.status(404).send('Error al procesar el punto de recogida');
+    }
+} 
 
   export function formularioEnvioProducto(req, res) {
     const { id } = req.params;
@@ -35,12 +51,17 @@ export function formularioPuntoRecogida(req, res) {
     const producto = Producto.getProductById(id);
     const usuario = Usuario.getUsuarioById(producto.id_user);
     const imagen = Imagen.getImagenByProductId(id);
+
+    const puntoRecogida = req.session.puntoRecogidaSeleccionado;
+    console.log("Punto de recogida seleccionado:", puntoRecogida);
+  
     const params = {
       contenido: "paginas/envios/resumenProducto",
       session: req.session,
       producto,
       usuario,
-      imagen
+      imagen,
+      puntoRecogida
     };
     res.render("pagina", params);
   }
