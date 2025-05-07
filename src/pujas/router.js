09754d4759ebc9a8bf88e25a3b6945d84b3c7aa4
carri8 <mcarri17@ucm.es>
2025-05-07 09:21:30 +0200

    Quitado pujar para arreglarlo

--- src/pujas/router.js
+++ src/pujas/router.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import { viewMisPujas, enviarPuja, verPujas, verPuja, iniciarOcrearPuja} from './controllers.js';
-import { autenticado } from '../middleware/auth.js'; // Asegúrate de tener esta autenticación
+import { viewMisPujas, enviarPuja, verPujas, verPuja, iniciarOcrearPuja, nuevaPuja} from './controllers.js';
 import { body } from 'express-validator';
 
 const pujaRouter = express.Router();
@@ -8,7 +7,7 @@ const pujaRouter = express.Router();
 // Rutas para ver las pujas, enviar una puja
 pujaRouter.get('/pujas', verPujas);
 pujaRouter.get('/puja/:id', verPuja);
-pujaRouter.get('/iniciarPuja/:id', iniciarOcrearPuja);
+pujaRouter.get('/crearPuja', nuevaPuja);
 pujaRouter.get('/misPujas', viewMisPujas);  // Ver todas las pujas del usuario
 pujaRouter.post('/enviarPuja', body('valor', 'La cantidad de la puja no puede estar vacía').notEmpty().isFloat({ gt: 0 }), enviarPuja); // Enviar una nueva puja
 
